0b7d162b1a00e19443fcc2f02549ed9e66fac9c2
David Raleigh <davidraleigh@gmail.com>
2017-07-16 17:38:50 -0700

    date and enum rename

--- epl/imagery/reader.py
+++ epl/imagery/reader.py
@@ -1,19 +1,12 @@
+import os
+import errno
+
 from enum import Enum
 from subprocess import call
 # Imports the Google Cloud client library
 from google.cloud import bigquery
 
-
-projectid = "geometry-service"
-
-
-import os
-import errno
-
-
-
-
-class SatelliteID(Enum):
+class SpacecraftID(Enum):
     LANDSAT_8 = 1
     LANDSAT_7 = 2
 
@@ -39,42 +32,39 @@ class Sentinel2:
 
 
 class Metadata:
+    def __init__(self):
+        self.m_client = bigquery.Client()
+        self.m_timeout_ms = 10000
 
     def search(self, satellite_id, bounding_box=None, start_date=None, end_date=None, sort_by=None, limit=10, sql_filters=None):
-        TIMEOUT_MS = 10000
-
-        client = bigquery.Client(project=projectid)
-
         # # Perform a synchronous query.
         query_builder = 'SELECT * FROM [bigquery-public-data:cloud_storage_geo_index.landsat_index] ' \
-                        'WHERE satellite_id={} '.format(satellite_id)
+                        'WHERE spacecraft_id="{}"'.format(satellite_id.name)
 
         if bounding_box is not None:
-            query_builder += 'AND bounding_box={}'.format(bounding_box)
+            # north_lat	south_lat	west_lon	east_lon
+            query_builder += ' AND '
+
+            query_builder += ' AND bounding_box={}'.format(bounding_box)
 
         if start_date is not None:
-            query_builder += 'AND start_date={}'.format(start_date)
+            query_builder += ' AND date_acquired>="{}"'.format(start_date.isoformat())
 
         if end_date is not None:
-            query_builder += 'AND end_date={}'.format(end_date)
+            query_builder += ' AND date_acquired<="{}"'.format(end_date.isoformat())
 
         if sql_filters is not None:
             for sql_filter in sql_filters:
                 print(sql_filter)
 
         if sort_by is not None:
-            query_builder += 'SORT BY {}'.format(sort_by)
+            query_builder += ' SORT BY {}'.format(sort_by)
 
-        query_builder = 'SELECT * FROM [bigquery-public-data:cloud_storage_geo_index.landsat_index] ' \
-                        'WHERE spacecraft_id="LANDSAT_8"'
-        query = client.run_sync_query('{} LIMIT {}'.format(query_builder, limit))
-        query.timeout_ms = TIMEOUT_MS
+        query = self.m_client.run_sync_query('{} LIMIT {}'.format(query_builder, limit))
+        query.timeout_ms = self.m_timeout_ms
         query.run()
 
-        val = []
-        for row in query.rows:
-            val.append(row)
-        return val
+        return query.rows
 
 
 class Storage:
