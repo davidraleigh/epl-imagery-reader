54a1a4ee6fc268ded20f97a3d0aa18cc3b62ff3d
David Raleigh <davidraleigh@gmail.com>
2017-11-03 11:34:29 -0700

    updating compile and setup scripts

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ old_path = os.getcwd()
 os.chdir(src_path)
 sys.path.insert(0, src_path)
 
-kwargs = {}
-kwargs['name'] = 'epl'
-kwargs['description'] = 'Echo Park Labs GCP Imagery Library'
-kwargs['long_description'] = open('README.md').read()
-kwargs['author'] = 'Echo Park Labs'
-kwargs['author_email'] = 'david@echoparklabs.com'
-kwargs['url'] = 'https://bitbucket.org/EchoParkLabs/gcp-imagery-reader'
-kwargs['version'] = '0.1dev'
-kwargs['packages'] = find_packages('.')
-
+kwargs = {
+    'name': 'epl',
+    'description': 'Echo Park Labs GCP Imagery Library',
+    'long_description': open('README.md').read(),
+    'author': 'Echo Park Labs',
+    'author_email': 'david@echoparklabs.com',
+    'url': 'https://bitbucket.org/EchoParkLabs/gcp-imagery-reader',
+    'version': open('VERSION').read(),
+    'packages': find_packages('.')
+}
 
 clssfrs = [
     "Programming Language :: Python",
