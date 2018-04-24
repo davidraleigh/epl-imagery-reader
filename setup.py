148d05c2eddd4c5fa2c667e6e107115f87139c46
David Raleigh <davidraleigh@gmail.com>
2018-04-24 13:25:45 -0700

    not zip_safe

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ kwargs = {
     'author_email': 'david@echoparklabs.com',
     'url': 'https://bitbucket.org/EchoParkLabs/gcp-imagery-reader',
     'version': open('VERSION').read(),
-    'packages': find_packages('.')
+    'packages': find_packages('.'),
+    'zip_safe': False
 }
 
 clssfrs = [
